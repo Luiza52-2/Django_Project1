10e9688e29e4611f41e46f432a38c61768a0a290
Luiza52-2 <nasykulova19@gmail.com>
2025-05-24 12:06:18 +0600

    Implemented filtering, search, and pagination for posts using SearchForm

--- posts/forms.py
+++ posts/forms.py
@@ -1,18 +1,16 @@
 from django import forms
-from .models import Post
-
+from .models import Category, Post
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['image', 'title', 'content', 'category', 'tags']
         widgets = {
-            'tags': forms.CheckboxSelectMultiple()  # нужно вызывать
+            'tags': forms.CheckboxSelectMultiple()
         }
         labels = {
             'title': 'Заголовок',
             'content': 'Контент',
-            'rate': 'Рейтинг',
             'category': 'Категория',
             'tags': 'Теги'
         }
@@ -32,24 +30,18 @@ class PostForm(forms.ModelForm):
             raise forms.ValidationError('Title and Content cannot be the same')
 
         return cleaned_data
+
+
+class SearchForm(forms.Form):
+    search = forms.CharField(max_length=256, required=False)
+    category_id = forms.ModelChoiceField(
+        queryset=Category.objects.all(), required=False)
     
-    class PostForm2(forms.ModelForm):
-        class Meta:
-            model = Post
-            fields = ['image', 'title', 'content', 'category', 'tags']
-        
-        def clean_title(self):
-            title = self.cleaned_data.get('title')
-            if title and title.lower() == 'python':
-                raise forms.ValidationError('Title cannot be "Python"')
-            return title
-
-        def clean(self):
-            cleaned_data = super().clean()
-            title = cleaned_data.get('title')
-            content = cleaned_data.get('content')
-
-            if title and content and title.lower() == content.lower():
-                raise forms.ValidationError('Title and Content cannot be the same')
-
-            return cleaned_data            
+    orderings = (
+        ('created_at', 'Creation date'),
+        ('-created_at', 'Creation date (desc)'),
+        ('rate', 'Rate'),
+        ('-rate', 'Rate (desc)'),
+        (None, 'No ordering')
+    )
+    ordering = forms.ChoiceField(choices=orderings, required=False)
--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, HttpResponse, redirect
 from random import randint
 from posts.models import Post
-from posts.forms import PostForm
+from posts.forms import PostForm, SearchForm
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 
 def index(request):
     return HttpResponse(f"Hello, world. {randint(1,100)}")
@@ -12,12 +13,36 @@ def homepage_view(request):
 
 @login_required(login_url="/login/")
 def post_list_view(request):
+    form = SearchForm(request.GET)
+    limit = 4
     if request.method == 'GET':
+        search = request.GET.get('search')
         posts = Post.objects.all()
-        return render(request, "posts/post_list.html", context={'posts': posts})
+        category_id = request.GET.get('category_id')
+        ordering = request.GET.get('ordering')
+        page = request.GET.get('page', 1)
+        if category_id:
+            posts = posts.filter(category_id=category_id)
+        if search:
+            posts = posts.filter (Q(title__icontains=search) | Q(content__icontains=search))
+        if ordering:
+            posts = posts.order_by(ordering)
+
+        max_pages = len(posts) // limit
+        if round(max_pages) < max_pages:
+            max_pages = round(max_pages) +1
+        else:
+            max_pages = round(max_pages)
+
+        start = (int(page) - 1) * limit
+        end = int(page) * limit
+        posts = posts[start:end]
+        context_data = {'posts': posts, 'form': form, 'max_pages':range(1, max_pages +1)}
+        return render(request, "posts/post_list.html", context=context_data)
 
 @login_required(login_url="/login/")
 def post_detail_view(request, post_id):
+    
     if request.method == 'GET':
         post = Post.objects.filter(id=post_id).first()
         return render(request, 'posts/post_detail.html', context={'post': post})
